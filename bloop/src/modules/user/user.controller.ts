0594d288ef67e37940b79d5d25813881d6b0d6c6
leoo9433335 <silvaleonardoduarte2222@gmail.com>
2025-05-31 10:44:56 -0300

    end-points users

--- bloop/src/modules/user/user.controller.ts
+++ bloop/src/modules/user/user.controller.ts
@@ -1,8 +1,24 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Get, Post } from "@nestjs/common";
 import { UserService } from "./user.service";
 
 
-@Controller("user")
+@Controller("user/")
 export class UserController {
     constructor(private readonly userService: UserService) {}
+    // Define your user-related endpoints here
+    // For example, you can add a method to get all users
+
+    @Get("listaUSUARIOS/")
+    async getAllUsers() {
+         return this.userService.getAllUsers();
+}
+    @Get("getUserById/:id")
+    async getUserById(id: number) {
+        return this.userService.getUserById(id);
+    }
+
+    @Post("createUser/")
+    async createUser(@Body() body: { name: string; manager: string; teamIds: number[] }) {
+        return this.userService.createUser(body);
+    }
 }
--- bloop/src/modules/user/user.service.ts
+++ bloop/src/modules/user/user.service.ts
@@ -1,7 +1,27 @@
 import { Injectable } from "@nestjs/common";
 import { PrismaService } from "../prisma/prisma.service";
+import { UserDto } from "./user.dto";
 
 @Injectable()
 export class UserService {
     constructor(private readonly prismaService: PrismaService) {}
+
+    async getAllUsers() {
+        return this.prismaService.user.findMany();
+    }
+    async getUserById(id: number) {
+        return this.prismaService.user.findUnique({
+            where: { id },
+        });
+    }
+    async createUser(body: { name: string; manager: string; teamIds: number[]; }) {
+        return this.prismaService.user.create({
+            data: {
+                name: body.name,
+                manager: body.manager,
+                teamIds: body.teamIds,
+            }
+        });
+    }
+
 }
