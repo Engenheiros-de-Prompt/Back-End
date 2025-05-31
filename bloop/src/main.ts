0b5059c33d850e23698532c10e05f9df04eba1fe
Gabriel Gerhardt <gabrielgerhardt27@gmail.com>
2025-05-31 17:14:06 -0300

    .

--- bloop/src/main.ts
+++ bloop/src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Habilitar CORS para todas as origens (ou especifique o frontend)
+  app.enableCors({
+    origin: 'http://localhost:5173', // Frontend que faz requisição
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
+  await app.listen(3000);
 }
 bootstrap();
