926cb0cd6c73bb5531e7ea07f351146943c68484
Gabriel Gerhardt <gabrielgerhardt27@gmail.com>
2025-05-31 17:33:09 -0300

    .

--- bloop/src/modules/user/user.controller.ts
+++ bloop/src/modules/user/user.controller.ts
@@ -1,25 +1,24 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Param } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserDto } from './dto/user.dto';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
-  // Define your user-related endpoints here
-  // For example, you can add a method to get all users
 
   @Get('getAllUsers')
   async getAllUsers() {
     return this.userService.getAllUsers();
   }
+
   @Get('getUserById/:id')
-  async getUserById(id: number) {
-    return this.userService.getUserById(id);
+  async getUserById(@Param('id') id: string) {
+    return this.userService.getUserById(+id); // Convertendo para número
   }
 
   @Get('getUsersByteamId/:teamId')
-  async getUsersByTeamId(teamId: number) {
-    return this.userService.getUsersByTeamId(teamId);
+  async getUsersByTeamId(@Param('teamId') teamId: string) {
+    return this.userService.getUsersByTeamId(+teamId);
   }
 
   @Post('createUser')
--- bloop/src/modules/user/user.service.ts
+++ bloop/src/modules/user/user.service.ts
@@ -1,33 +1,33 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { UserDto } from './dto/user.dto';
-import { User } from '@prisma/client';
 
 @Injectable()
 export class UserService {
   constructor(private readonly prismaService: PrismaService) {}
 
   async getUsersByTeamId(teamId: number) {
-    let teamusers = this.prismaService.teamUser.findMany({
-        where: { teamId },
-        include: { user: true },
-    })
-
-    return teamusers.then((teamUsers) => {
-      return teamUsers.map((teamUser) => teamUser.user);
+    const teamUsers = await this.prismaService.teamUser.findMany({
+      where: { teamId },
+      include: { user: true },
     });
+
+    return teamUsers.map((teamUser) => teamUser.user);
   }
 
   async getAllUsers() {
     return this.prismaService.user.findMany();
   }
+
   async getUserById(id: number) {
     return this.prismaService.user.findUnique({
       where: { id },
     });
   }
+
   async createUser(body: UserDto) {
     const teamsIds = body.teamIds || [];
+
     const teams = await Promise.all(
       teamsIds.map((teamId) =>
         this.prismaService.team.findUnique({
@@ -35,13 +35,18 @@ export class UserService {
         }),
       ),
     );
-    const validTeams = teams.filter((team) => team !== null);
+
+    const validTeams = teams.filter((team) => team !== null) as {
+      id: number;
+    }[];
+
     const user = await this.prismaService.user.create({
       data: {
         name: body.name,
         Manager: Boolean(body.manager),
       },
     });
+
     await Promise.all(
       validTeams.map((team) =>
         this.prismaService.teamUser.create({
@@ -52,6 +57,7 @@ export class UserService {
         }),
       ),
     );
+
     return this.prismaService.user.findUnique({
       where: { id: user.id },
       include: { teams: true },
